b374e26db320a85787ea79cebc95ec370e5545b1
preet4737 <preet4737@gmail.com>
2020-02-02 06:31:52 +0530

    Navbar Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState}from 'react';
 import {
   Navbar,
   NavbarBrand,
@@ -7,6 +7,8 @@ import {
   DropdownMenu,
   DropdownToggle,
   Dropdown,
+  Row,
+  Col
 } from 'reactstrap';
 
 import { Link, Redirect, useHistory } from 'react-router-dom';
@@ -18,38 +20,43 @@ import NavbarLoginSignUp from './NavbarLoginSignUp'
 const CustomNavbar = () => {
   const user = getUserDetails();
   const history = useHistory()
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(prevState => !prevState);
   console.log(user)
  
     return(
       (user !== null)
       ?(
-      <Navbar color="primary" dark>
+      <Navbar color="primary" dark light expand="lg">
       {user.type.typeName === 'volunteer' ? (
-        <Button className="mr-auto" tag={Link} to="/create_checkpoint">Volunteer</Button>
-      ): <Button className="mr-auto" tag={Link} to="/donate/request">Volunteer</Button>
+        <Button className="mr-auto" tag={Link} to="/create_checkpoint">CreateCheckpoint</Button>
+      ): <Button className="mr-auto" tag={Link} to="/donate/request">Donate</Button>
       };
 
-      <NavbarBrand tag={Link} to="/home">FoodFeed</NavbarBrand>
-
+      <NavbarBrand tag={Link} to="/">FoodFeed</NavbarBrand>
+      <Row xs="4">
+        <Col></Col>
       {user.type.typeName === 'volunteer'
-        ?(<Dropdown>
+        ?(<Col><Dropdown isOpen={dropdownOpen} toggle={toggle} style={{marginRight:"4rem"}}>
             <DropdownToggle caret>Nearby</DropdownToggle>
               <DropdownMenu>
                 <DropdownItem tag={Link} to="/donation_request">Restaurant</DropdownItem>
                 <DropdownItem divider />
                 <DropdownItem tag={Link} to="/checkpoint">Checkpoint</DropdownItem>
             </DropdownMenu>
-          </Dropdown>)
+          </Dropdown></Col>)
         : null
       }
-      <Button className="ml-auto" tag={Link} onClick={()=>{
+      <Col><Button style={{marginRight:"0rem"}}tag={Link} className="" onClick={()=>{
         localStorage.clear()
-        history.push('/')
-      }}>LogOut</Button>
-      <Button className="ml-auto" tage={Link} to="/profile">Profile</Button>
+        history.push('/login')
+      }}>Logout</Button></Col>
+      <Col><Button className="ml-auto" tage={Link} to="/profile">Profile</Button></Col>
+      </Row>
     </Navbar>)
     :
-    <Redirect to="/"/>
+    <Redirect to="/login"/>
     
   ); 
   
